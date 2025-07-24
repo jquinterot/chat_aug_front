18144669b5d1d4681d90b9db9b28e367f9c33739
Johany Quintero <johandquintero@utp.edu.co>
2025-07-24 13:17:05 -0500

    fix: update MessageBubble to use shared Message type from @/types

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -1,11 +1,5 @@
+import { Message } from "@/types";
 import Avatar from "./Avatar";
-
-interface Message {
-  id: string;
-  content: string;
-  role: "user" | "assistant";
-  timestamp: Date;
-}
 
 interface MessageBubbleProps {
   message: Message;
